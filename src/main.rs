a53de51d396ddc15ae621ee3e41178866172565c
iam326 <takahashi.developer@gmail.com>
2020-07-12 11:50:19 +0900

    implement L-tika

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+const GPIO_LED: u8 = 23;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    let mut counter = 0;
+
+    while counter < 10 {
+        pin.set_high();
+        thread::sleep(Duration::from_millis(500));
+        pin.set_low();
+        thread::sleep(Duration::from_millis(500));
+        counter += 1;
+    }
+
+    Ok(())
 }
