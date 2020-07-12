d6cc34fae44de527a9a9a01eb69040f7049ca5d7
iam326 <takahashi.developer@gmail.com>
2020-07-12 13:28:24 +0900

    implement the process of reading temperature

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 use std::error::Error;
-use std::thread;
-use std::time::Duration;
 
-use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
+use rppal::i2c::I2c;
 
-const GPIO_LED: u8 = 23;
+const ADDRESS_ADT7410: u16 = 0x48;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
-    let mut counter = 0;
+    let mut i2c = I2c::with_bus(1).expect("Couldn't start i2c. Is the interface enabled?");
+    i2c.set_slave_address(ADDRESS_ADT7410).unwrap();
 
-    while counter < 10 {
-        pin.set_high();
-        thread::sleep(Duration::from_millis(500));
-        pin.set_low();
-        thread::sleep(Duration::from_millis(500));
-        counter += 1;
-    }
+    let temp = read_temperature(&i2c);
+    println!("{}", temp);
 
     Ok(())
+}
+
+fn read_temperature(i2c: &I2c) -> f32 {
+    let word = i2c.smbus_read_word(0x00).unwrap();
+    let mut data = ((word & 0xff00)>>8 | (word & 0xff) << 8) >> 3;
+
+    if data & 0x1000 == 0 {
+      data as f32 * 0.0625
+    } else {
+      ((!data & 0x1fff) + 1) as f32 * -0.0625
+    }
 }
