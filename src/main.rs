b4d1fa764f0e5b21079fd1ef7675a2c6be03047a
iam326 <takahashi.developer@gmail.com>
2020-07-19 15:09:25 +0900

    repeat the process to get the temperature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::thread;
+use std::time::Duration;
 
 use rppal::system::DeviceInfo;
 use rppal::i2c::I2c;
@@ -11,15 +13,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut i2c = I2c::with_bus(1).expect("Couldn't start i2c. Is the interface enabled?");
     i2c.set_slave_address(ADDRESS_ADT7410).unwrap();
 
-    let temp = read_temperature(&i2c);
-    println!("{}", temp);
-
-    Ok(())
+    loop {
+       let temp = read_temperature(&i2c);
+       println!("{}", temp);
+       thread::sleep(Duration::from_millis(1000));
+    }
 }
 
 fn read_temperature(i2c: &I2c) -> f32 {
     let word = i2c.smbus_read_word(0x00).unwrap();
-    let mut data = ((word & 0xff00)>>8 | (word & 0xff) << 8) >> 3;
+    let data = ((word & 0xff00)>>8 | (word & 0xff) << 8) >> 3;
 
     if data & 0x1000 == 0 {
       data as f32 * 0.0625
