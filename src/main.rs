29b77da623f007225f9f082bdd9f9010e1d3a8af
iam326 <takahashi.developer@gmail.com>
2020-07-22 07:57:34 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use rumqtt::{MqttClient, MqttOptions, QoS, ReconnectOptions};
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct SendData {
+struct Payload {
     timestamp: i64,
     temperature: f32,
 }
 
 const ADDRESS_ADT7410: u16 = 0x48;
+const ADDRESS_REGISTER: u8 = 0x00;
+const BUS: u8 = 1;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let client_id = env::var("AWS_IOT_CLIENT_ID").expect("AWS_IOT_CLIENT_ID is undefined.");
@@ -28,7 +30,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
     mqtt_client.subscribe("iot/topic", QoS::AtLeastOnce).unwrap();
 
-    let mut i2c = I2c::with_bus(1).expect("Couldn't start i2c. Is the interface enabled?");
+    let mut i2c = I2c::with_bus(BUS).expect("Couldn't start i2c. Is the interface enabled?");
     i2c.set_slave_address(ADDRESS_ADT7410).unwrap();
 
     let sleep_time = Duration::from_secs(10);
@@ -37,9 +39,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             let dt: DateTime<Local> = Local::now();
             let timestamp = dt.timestamp();
             let temp = read_temperature(&i2c);
-            let data = SendData { timestamp: timestamp, temperature: temp };
-            let payload = serde_json::to_string(&data).unwrap();
-            mqtt_client.publish("iot/topic", QoS::AtLeastOnce, false, payload).unwrap();
+            let payload = Payload { timestamp: timestamp, temperature: temp };
+            let payload_str = serde_json::to_string(&payload).unwrap();
+
+            mqtt_client.publish("iot/topic", QoS::AtLeastOnce, false, payload_str).unwrap();
             thread::sleep(sleep_time);
         }
     });
@@ -52,8 +55,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn read_temperature(i2c: &I2c) -> f32 {
-    let word = i2c.smbus_read_word(0x00).unwrap();
-    let data = ((word & 0xff00)>>8 | (word & 0xff) << 8) >> 3;
+    let word = i2c.smbus_read_word(ADDRESS_REGISTER).unwrap();
+    let data = ((word & 0xff00) >> 8 | (word & 0xff) << 8) >> 3;
 
     if data & 0x1000 == 0 {
       data as f32 * 0.0625
